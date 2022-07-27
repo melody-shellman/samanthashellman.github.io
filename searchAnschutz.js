6072a3e23237af36dad7fbfe974d562e1f92e69d
samanthashellman <sash9933@colorado.edu>
2022-07-27 17:31:56 -0600

    R1 G6

--- searchAnschutz.js
+++ searchAnschutz.js
@@ -3,7 +3,8 @@
 function findMotifs() {
     var sequence = document.getElementById("proteins").value;
     cleanedSeq = modify(sequence)
-    document.getElementById("test").innerHTML = cleanedSeq;
+    results = search(cleanedSeq)
+    document.getElementById("test").innerHTML = results;
   }
 
 
@@ -18,4 +19,17 @@ function modify(seq) {
     }
   
     return new_seq;
+  }
+
+
+  function search(proteins) {
+    var motifs = []
+
+    for (var i = 0, _pj_a = proteins.length-5; i < _pj_a; i += 1) {
+      if (proteins[i] == 'R' && proteins[i+5] == 'G'){
+        motifs.push(proteins.substring(i, i+6))
+      }
+    }
+
+    return motifs
   }
