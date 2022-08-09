711423ce8c0bfa33b0ed4b9239f94456e79328d4
samanthashellman <sash9933@colorado.edu>
2022-08-09 16:21:41 -0600

    scoring function

--- searchAnschutz.js
+++ searchAnschutz.js
@@ -1,16 +1,31 @@
 
 // called when "Find Motifs!" on anschutz tab button is pressed
 function findMotifs() {
-    var sequence = document.getElementById("proteins").value;
-    cleanedSeq = modify(sequence)
-    results = search(cleanedSeq)
-    document.getElementById("test").innerHTML = results;
+  var sequence = document.getElementById("proteins").value;
+  cleanedSeq = modify(sequence)
+  results = search(cleanedSeq)
+  document.getElementById("canonical").innerHTML = results;
 }
 
-
 function clearText() {
-  document.getElementById("test").innerHTML = '';
+  document.getElementById("canonical").innerHTML = '';
   document.getElementById("proteins").value = '';
+}
+
+function scoreMotifs(){
+  var mot = document.getElementById("8proteins").value;
+  if (mot.length != 8){
+    document.getElementById("score").innerHTML = "Error- sequence must be 8 characters long";
+  }
+  else{
+    document.getElementById("score").innerHTML = mot + ' -> score of ' + getScore(mot);
+  }
+  
+}
+
+function clearText2() {
+  document.getElementById("score").innerHTML = '';
+  document.getElementById("8proteins").value = '';
 }
 
 
@@ -30,7 +45,7 @@ function modify(seq) {
 
   function search(proteins) {
     //var motifs = ''
-    var motifs = 'Motif, Position, Score \<br>'
+    var motifs = 'Motif, &emsp; Position, &emsp; Score \<br>'
     var scores = []
     var key_value = [];
     //key_value = {};
@@ -43,410 +58,11 @@ function modify(seq) {
       if (proteins[i] == 'R' && proteins[i+5] == 'G'){
 
         thisMotif = proteins.substring(i, i+8);
-        thisScore = 6; // +3 each for R1 and G6
-        
-        // second position scoring
-        switch(thisMotif[1]) {
-          case 'P':
-            thisScore += 3;
-            break;
-          case 'G':
-            thisScore += 3;
-            break;
-          case 'A':
-            thisScore += 2;
-            break;
-          case 'V':
-            thisScore += 2;
-            break;
-          case 'L':
-            thisScore += 3;
-            break;
-          case 'I':
-            thisScore += 2;
-            break;
-          case 'M':
-            thisScore += 3;
-            break;
-          case 'C':
-            thisScore += 2;
-            break;
-          case 'S':
-            thisScore += 3;
-            break;
-          case 'T':
-            thisScore += 3;
-            break;
-          case 'R':
-            thisScore += 1;
-            break;
-          case 'K':
-            thisScore += 1;
-            break;
-          case 'H':
-            thisScore += 2;
-            break;
-          case 'D':
-            thisScore += 3;
-            break;
-          case 'E':
-            thisScore += 3;
-            break;
-          case 'N':
-            thisScore += 1;
-            break;
-          case 'Q':
-            thisScore += 3;
-            break;
-          case 'W':
-            thisScore += 2;
-            break;
-          case 'F':
-            thisScore += 2;
-            break;
-          case 'Y':
-            thisScore += 3;
-            break;
-          default:
-            thisScore += 0;
-        }
-
-
-        // third position scoring
-        switch(thisMotif[2]) {
-          case 'P':
-            thisScore += 3;
-            break;
-          case 'G':
-            thisScore += 2;
-            break;
-          case 'A':
-            thisScore += 3;
-            break;
-          case 'V':
-            thisScore += 2;
-            break;
-          case 'L':
-            thisScore += 2;
-            break;
-          case 'I':
-            thisScore += 1;
-            break;
-          case 'M':
-            thisScore += 3;
-            break;
-          case 'C':
-            thisScore += 2;
-            break;
-          case 'S':
-            thisScore += 3;
-            break;
-          case 'T':
-            thisScore += 2;
-            break;
-          case 'R':
-            thisScore += 3;
-            break;
-          case 'K':
-            thisScore += 2;
-            break;
-          case 'H':
-            thisScore += 2;
-            break;
-          case 'D':
-            thisScore += 3;
-            break;
-          case 'E':
-            thisScore += 3;
-            break;
-          case 'N':
-            thisScore += 2;
-            break;
-          case 'Q':
-            thisScore += 3;
-            break;
-          case 'W':
-            thisScore += 2;
-            break;
-          case 'F':
-            thisScore += -3;
-            break;
-          case 'Y':
-            thisScore += 1;
-            break;
-          default:
-            thisScore += 0;
-        }
-
-        // fourth position scoring
-        switch(thisMotif[3]) {
-          case 'P':
-            thisScore += 2;
-            break;
-          case 'G':
-            thisScore += 3;
-            break;
-          case 'A':
-            thisScore += 1;
-            break;
-          case 'V':
-            thisScore += -3;
-            break;
-          case 'L':
-            thisScore += -3;
-            break;
-          case 'I':
-            thisScore += -3;
-            break;
-          case 'M':
-            thisScore += -3;
-            break;
-          case 'C':
-            thisScore += -1;
-            break;
-          case 'S':
-            thisScore += -3;
-            break;
-          case 'T':
-            thisScore += -3;
-            break;
-          case 'R':
-            thisScore += -3;
-            break;
-          case 'K':
-            thisScore += -3;
-            break;
-          case 'H':
-            thisScore += -3;
-            break;
-          case 'D':
-            thisScore += -3;
-            break;
-          case 'E':
-            thisScore += -3;
-            break;
-          case 'N':
-            thisScore += -3;
-            break;
-          case 'Q':
-            thisScore += -3;
-            break;
-          case 'W':
-            thisScore += -3;
-            break;
-          case 'F':
-            thisScore += -3;
-            break;
-          case 'Y':
-            thisScore += -3;
-            break;
-          default:
-            thisScore += 0;
-        }
-
-        // fifth position scoring
-        switch(thisMotif[4]) {
-          case 'P':
-            thisScore += -3;
-            break;
-          case 'G':
-            thisScore += -3;
-            break;
-          case 'A':
-            thisScore += -3;
-            break;
-          case 'V':
-            thisScore += -1;
-            break;
-          case 'L':
-            thisScore += -3;
-            break;
-          case 'I':
-            thisScore += -1;
-            break;
-          case 'M':
-            thisScore += -3;
-            break;
-          case 'C':
-            thisScore += -2;
-            break;
-          case 'S':
-            thisScore += -3;
-            break;
-          case 'T':
-            thisScore += -3;
-            break;
-          case 'R':
-            thisScore += -3;
-            break;
-          case 'K':
-            thisScore += -3;
-            break;
-          case 'H':
-            thisScore += -3;
-            break;
-          case 'D':
-            thisScore += 3;
-            break;
-          case 'E':
-            thisScore += 2;
-            break;
-          case 'N':
-            thisScore += -3;
-            break;
-          case 'Q':
-            thisScore += -1;
-            break;
-          case 'W':
-            thisScore += -3;
-            break;
-          case 'F':
-            thisScore += -3;
-            break;
-          case 'Y':
-            thisScore += -1;
-            break;
-          default:
-            thisScore += 0;
-        }
-
-        // sixth position must be G
-
-        // seventh position scoring
-        switch(thisMotif[6]) {
-          case 'P':
-            thisScore += -3;
-            break;
-          case 'G':
-            thisScore += 2;
-            break;
-          case 'A':
-            thisScore += 3;
-            break;
-          case 'V':
-            thisScore += 2;
-            break;
-          case 'L':
-            thisScore += 1;
-            break;
-          case 'I':
-            thisScore += 2;
-            break;
-          case 'M':
-            thisScore += 3;
-            break;
-          case 'C':
-            thisScore += 3;
-            break;
-          case 'S':
-            thisScore += 3;
-            break;
-          case 'T':
-            thisScore += 3;
-            break;
-          case 'R':
-            thisScore += 2;
-            break;
-          case 'K':
-            thisScore += 2;
-            break;
-          case 'H':
-            thisScore += 3;
-            break;
-          case 'D':
-            thisScore += 3;
-            break;
-          case 'E':
-            thisScore += 3;
-            break;
-          case 'N':
-            thisScore += 3;
-            break;
-          case 'Q':
-            thisScore += 3;
-            break;
-          case 'W':
-            thisScore += 2;
-            break;
-          case 'F':
-            thisScore += 2;
-            break;
-          case 'Y':
-            thisScore += 2;
-            break;
-          default:
-            thisScore += 0;
-        }
-
-        // eighth position scoring
-        switch(thisMotif[7]) {
-          case 'P':
-            thisScore += 2;
-            break;
-          case 'G':
-            thisScore += 1;
-            break;
-          case 'A':
-            thisScore += 2;
-            break;
-          case 'V':
-            thisScore += 1;
-            break;
-          case 'L':
-            thisScore += 1;
-            break;
-          case 'I':
-            thisScore += 1;
-            break;
-          case 'M':
-            thisScore += 1;
-            break;
-          case 'C':
-            thisScore += 2;
-            break;
-          case 'S':
-            thisScore += 2;
-            break;
-          case 'T':
-            thisScore += 1;
-            break;
-          case 'R':
-            thisScore += -2;
-            break;
-          case 'K':
-            thisScore += -2;
-            break;
-          case 'H':
-            thisScore += 1;
-            break;
-          case 'D':
-            thisScore += 3;
-            break;
-          case 'E':
-            thisScore += 3;
-            break;
-          case 'N':
-            thisScore += 2;
-            break;
-          case 'Q':
-            thisScore += 1;
-            break;
-          case 'W':
-            thisScore += -1;
-            break;
-          case 'F':
-            thisScore += 1;
-            break;
-          case 'Y':
-            thisScore += 1;
-            break;
-          default:
-            thisScore += 0;
-        }
+        thisScore = getScore(thisMotif); 
 
         realIndex = i + 1
         
-        motifs = motifs + thisMotif + ', ' + realIndex + ', ' + thisScore + '\<br>';
+        motifs = motifs + thisMotif + ', &emsp;' + realIndex + ', &emsp; ' + thisScore + '\<br>';
 
         
         //var cont = true;
@@ -470,3 +86,425 @@ function modify(seq) {
 
     return motifs;
   };
+
+
+  function getScore(thisMotif){
+    thisScore = 0;
+
+    // first position scoring
+    switch(thisMotif[0]){
+      case 'R':
+        thisScore += 3;
+        break;
+      default:
+        thisScore += -3;
+    }
+
+    // second position scoring
+    switch(thisMotif[1]) {
+      case 'P':
+        thisScore += 3;
+        break;
+      case 'G':
+        thisScore += 3;
+        break;
+      case 'A':
+        thisScore += 2;
+        break;
+      case 'V':
+        thisScore += 2;
+        break;
+      case 'L':
+        thisScore += 3;
+        break;
+      case 'I':
+        thisScore += 2;
+        break;
+      case 'M':
+        thisScore += 3;
+        break;
+      case 'C':
+        thisScore += 2;
+        break;
+      case 'S':
+        thisScore += 3;
+        break;
+      case 'T':
+        thisScore += 3;
+        break;
+      case 'R':
+        thisScore += 1;
+        break;
+      case 'K':
+        thisScore += 1;
+        break;
+      case 'H':
+        thisScore += 2;
+        break;
+      case 'D':
+        thisScore += 3;
+        break;
+      case 'E':
+        thisScore += 3;
+        break;
+      case 'N':
+        thisScore += 1;
+        break;
+      case 'Q':
+        thisScore += 3;
+        break;
+      case 'W':
+        thisScore += 2;
+        break;
+      case 'F':
+        thisScore += 2;
+        break;
+      case 'Y':
+        thisScore += 3;
+        break;
+      default:
+        thisScore += 0;
+    }
+
+
+    // third position scoring
+    switch(thisMotif[2]) {
+      case 'P':
+        thisScore += 3;
+        break;
+      case 'G':
+        thisScore += 2;
+        break;
+      case 'A':
+        thisScore += 3;
+        break;
+      case 'V':
+        thisScore += 2;
+        break;
+      case 'L':
+        thisScore += 2;
+        break;
+      case 'I':
+        thisScore += 1;
+        break;
+      case 'M':
+        thisScore += 3;
+        break;
+      case 'C':
+        thisScore += 2;
+        break;
+      case 'S':
+        thisScore += 3;
+        break;
+      case 'T':
+        thisScore += 2;
+        break;
+      case 'R':
+        thisScore += 3;
+        break;
+      case 'K':
+        thisScore += 2;
+        break;
+      case 'H':
+        thisScore += 2;
+        break;
+      case 'D':
+        thisScore += 3;
+        break;
+      case 'E':
+        thisScore += 3;
+        break;
+      case 'N':
+        thisScore += 2;
+        break;
+      case 'Q':
+        thisScore += 3;
+        break;
+      case 'W':
+        thisScore += 2;
+        break;
+      case 'F':
+        thisScore += -3;
+        break;
+      case 'Y':
+        thisScore += 1;
+        break;
+      default:
+        thisScore += 0;
+    }
+
+    // fourth position scoring
+    switch(thisMotif[3]) {
+      case 'P':
+        thisScore += 2;
+        break;
+      case 'G':
+        thisScore += 3;
+        break;
+      case 'A':
+        thisScore += 1;
+        break;
+      case 'V':
+        thisScore += -3;
+        break;
+      case 'L':
+        thisScore += -3;
+        break;
+      case 'I':
+        thisScore += -3;
+        break;
+      case 'M':
+        thisScore += -3;
+        break;
+      case 'C':
+        thisScore += -1;
+        break;
+      case 'S':
+        thisScore += -3;
+        break;
+      case 'T':
+        thisScore += -3;
+        break;
+      case 'R':
+        thisScore += -3;
+        break;
+      case 'K':
+        thisScore += -3;
+        break;
+      case 'H':
+        thisScore += -3;
+        break;
+      case 'D':
+        thisScore += -3;
+        break;
+      case 'E':
+        thisScore += -3;
+        break;
+      case 'N':
+        thisScore += -3;
+        break;
+      case 'Q':
+        thisScore += -3;
+        break;
+      case 'W':
+        thisScore += -3;
+        break;
+      case 'F':
+        thisScore += -3;
+        break;
+      case 'Y':
+        thisScore += -3;
+        break;
+      default:
+        thisScore += 0;
+    }
+
+    // fifth position scoring
+    switch(thisMotif[4]) {
+      case 'P':
+        thisScore += -3;
+        break;
+      case 'G':
+        thisScore += -3;
+        break;
+      case 'A':
+        thisScore += -3;
+        break;
+      case 'V':
+        thisScore += -1;
+        break;
+      case 'L':
+        thisScore += -3;
+        break;
+      case 'I':
+        thisScore += -1;
+        break;
+      case 'M':
+        thisScore += -3;
+        break;
+      case 'C':
+        thisScore += -2;
+        break;
+      case 'S':
+        thisScore += -3;
+        break;
+      case 'T':
+        thisScore += -3;
+        break;
+      case 'R':
+        thisScore += -3;
+        break;
+      case 'K':
+        thisScore += -3;
+        break;
+      case 'H':
+        thisScore += -3;
+        break;
+      case 'D':
+        thisScore += 3;
+        break;
+      case 'E':
+        thisScore += 2;
+        break;
+      case 'N':
+        thisScore += -3;
+        break;
+      case 'Q':
+        thisScore += -1;
+        break;
+      case 'W':
+        thisScore += -3;
+        break;
+      case 'F':
+        thisScore += -3;
+        break;
+      case 'Y':
+        thisScore += -1;
+        break;
+      default:
+        thisScore += 0;
+    }
+
+    // sixth position scoring
+    switch(thisMotif[5]) {
+      case 'G':
+        thisScore += 3;
+        break;
+      default:
+        thisScore += -3;
+    }
+
+    // seventh position scoring
+    switch(thisMotif[6]) {
+      case 'P':
+        thisScore += -3;
+        break;
+      case 'G':
+        thisScore += 2;
+        break;
+      case 'A':
+        thisScore += 3;
+        break;
+      case 'V':
+        thisScore += 2;
+        break;
+      case 'L':
+        thisScore += 1;
+        break;
+      case 'I':
+        thisScore += 2;
+        break;
+      case 'M':
+        thisScore += 3;
+        break;
+      case 'C':
+        thisScore += 3;
+        break;
+      case 'S':
+        thisScore += 3;
+        break;
+      case 'T':
+        thisScore += 3;
+        break;
+      case 'R':
+        thisScore += 2;
+        break;
+      case 'K':
+        thisScore += 2;
+        break;
+      case 'H':
+        thisScore += 3;
+        break;
+      case 'D':
+        thisScore += 3;
+        break;
+      case 'E':
+        thisScore += 3;
+        break;
+      case 'N':
+        thisScore += 3;
+        break;
+      case 'Q':
+        thisScore += 3;
+        break;
+      case 'W':
+        thisScore += 2;
+        break;
+      case 'F':
+        thisScore += 2;
+        break;
+      case 'Y':
+        thisScore += 2;
+        break;
+      default:
+        thisScore += 0;
+    }
+
+    // eighth position scoring
+    switch(thisMotif[7]) {
+      case 'P':
+        thisScore += 2;
+        break;
+      case 'G':
+        thisScore += 1;
+        break;
+      case 'A':
+        thisScore += 2;
+        break;
+      case 'V':
+        thisScore += 1;
+        break;
+      case 'L':
+        thisScore += 1;
+        break;
+      case 'I':
+        thisScore += 1;
+        break;
+      case 'M':
+        thisScore += 1;
+        break;
+      case 'C':
+        thisScore += 2;
+        break;
+      case 'S':
+        thisScore += 2;
+        break;
+      case 'T':
+        thisScore += 1;
+        break;
+      case 'R':
+        thisScore += -2;
+        break;
+      case 'K':
+        thisScore += -2;
+        break;
+      case 'H':
+        thisScore += 1;
+        break;
+      case 'D':
+        thisScore += 3;
+        break;
+      case 'E':
+        thisScore += 3;
+        break;
+      case 'N':
+        thisScore += 2;
+        break;
+      case 'Q':
+        thisScore += 1;
+        break;
+      case 'W':
+        thisScore += -1;
+        break;
+      case 'F':
+        thisScore += 1;
+        break;
+      case 'Y':
+        thisScore += 1;
+        break;
+      default:
+        thisScore += 0;
+    }
+
+    return thisScore;
+  }
